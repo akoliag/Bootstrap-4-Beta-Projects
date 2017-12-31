f3dc93db1d1a0165659bde4447c0dd9a580cb9bd
Aga <a.kolia.grudzien@gmail.com>
2017-12-31 21:19:35 +0100

    add photo gallery and newsletter

--- glozzom/src/js/main.js
+++ glozzom/src/js/main.js
@@ -10,3 +10,9 @@ $(function() {
     });
   });
 });
+
+//ekkoLightBox to Photo Gallery
+$(document).on('click', '[data-toggle="lightbox"]', function(event){
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
